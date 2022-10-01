f2be9ac6ac217b5309ed6dc25985861af35df89c
Akira Mizuno <akmizno@gmail.com>
2022-10-02 02:29:12 +0900

    Update Snapshot and SnapshotHandler

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,28 +1,41 @@
 use std::marker::PhantomData;
+pub trait Snapshot {
+    type Snapshot;
+    fn to_snapshot(&self) -> Self::Snapshot;
+    fn from_snapshot(snapshot: &Self::Snapshot) -> Self;
+}
 
-pub trait SnapshotMaker {
+pub(crate) trait SnapshotHandler {
     type State;
     type Snapshot;
-    fn to_snapshot(&self, state: &Self::State) -> Self::Snapshot;
-    fn from_snapshot(&self, snapshot: &Self::Snapshot) -> Self::State;
+    fn to_snapshot(state: &Self::State) -> Self::Snapshot;
+    fn from_snapshot(snapshot: &Self::Snapshot) -> Self::State;
 }
 
 #[derive(Clone, Debug)]
-pub struct CloneMaker<T>(PhantomData<T>);
+pub(crate) struct CloneSnapshot<T>(PhantomData<T>);
 
-impl<T> CloneMaker<T> {
-    pub const fn new() -> Self {
-        Self(PhantomData::<T>)
+impl<T: Clone> SnapshotHandler for CloneSnapshot<T> {
+    type State = T;
+    type Snapshot = T;
+    fn to_snapshot(state: &Self::State) -> Self::Snapshot {
+        state.clone()
+    }
+    fn from_snapshot(snapshot: &Self::Snapshot) -> Self::State {
+        snapshot.clone()
     }
 }
 
-impl<T: Clone> SnapshotMaker for CloneMaker<T> {
+#[derive(Clone, Debug)]
+pub(crate) struct TraitSnapshot<T: Snapshot<Snapshot = S>, S>(PhantomData<T>);
+
+impl<T: Snapshot<Snapshot = S>, S> SnapshotHandler for TraitSnapshot<T, S> {
     type State = T;
-    type Snapshot = T;
-    fn to_snapshot(&self, state: &Self::State) -> Self::Snapshot {
-        state.clone()
+    type Snapshot = S;
+    fn to_snapshot(state: &Self::State) -> Self::Snapshot {
+        state.to_snapshot()
     }
-    fn from_snapshot(&self, snapshot: &Self::Snapshot) -> Self::State {
-        snapshot.clone()
+    fn from_snapshot(snapshot: &Self::Snapshot) -> Self::State {
+        Self::from_snapshot(snapshot)
     }
 }
